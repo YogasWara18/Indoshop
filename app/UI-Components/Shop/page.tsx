f8763134c17a6f00e11dc3f39098c2c6575fceda
YogasWara18 <teukukurniayogaswara@gmail.com>
2026-01-26 15:04:35 +0700

    Update fix done

--- app/UI-Components/Shop/page.tsx
+++ app/UI-Components/Shop/page.tsx
@@ -10,25 +10,40 @@ import Recommend from "@/app/JsonData/Recommend.json";
 import ProductDetails from "./ProductDetails/ProductDetails";
 import Products from "./Products/Products";
 
+
+type ProductType = {
+  Id: string;
+  image: string;
+  title: string;
+  price: string;
+  lessprice: string;
+  review: string;
+  sold: string;
+};
+
 export default function ShopPage() {
-  const allProducts = [
+  const allProducts: ProductType[] = [
     ...BestDeals,
     ...BestSales,
     ...HotDeals,
     ...TopProduct,
     ...Recommend,
-  ];
+  ].map((p) => ({
+    Id: p.id, // ubah ke huruf besar
+    image: p.image,
+    title: p.title,
+    price: p.price,
+    lessprice: p.lessprice,
+    review: p.review,
+    sold: p.sold,
+  }));
 
   const searchParams = useSearchParams();
-  const productId = searchParams.get("id");
+  const productId = searchParams.get("id") ?? undefined;
 
-  return (
-    <div className="">
-      {productId ? (
-        <ProductDetails id={productId} products={allProducts} />
-      ) : (
-        <Products />
-      )}
-    </div>
+  return productId ? (
+    <ProductDetails id={productId} products={allProducts} />
+  ) : (
+    <Products />
   );
 }
